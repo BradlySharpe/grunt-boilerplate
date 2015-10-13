9bd889a92ecbbf6c9ef2587d5bd50b49f94c7e72
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-10-13 18:16:26 +1100

    Add default Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+  module.exports = function(grunt) {
+    require('time-grunt')(grunt);
+    require('load-grunt-config')(grunt, {
+      configPath: require('path').join(process.cwd(), 'grunt/config'),
+      jitGrunt: {
+        customTasksDir: 'grunt/tasks',
+        staticMappings: { }
+      },
+      data: {
+        config: grunt.file.readJSON('./configuration.json'),
+        pkg: grunt.file.readJSON('./package.json')
+      }
+    });
+  };
+}());
