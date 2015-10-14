095b57aa77cac954131edf8d1fa6b25f3e3ef96e
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-10-14 13:45:07 +1100

    Add watch tasks

--- grunt/config/watch.js
+++ grunt/config/watch.js
@@ -8,6 +8,22 @@ module.exports = {
     files: ['<%= config.directories.source %>**/*.hbs'],
     tasks: [ 'compile-html' ]
   },
+  'css-partials': {
+    files: ['<%= config.directories.partials %>css/**/*.scss'],
+    tasks: [ 'compile-css-partials', 'compile-html' ]
+  },
+  'css-sass': {
+    files: ['<%= config.directories.sass %>**/*.scss'],
+    tasks: [ 'compile-css-sass' ]
+  },
+  js: {
+    files: ['<%= config.directories.scripts %>**/*.js'],
+    tasks: [ 'compile-js' ]
+  },
+  project: {
+    files: ['grunt/**/*.js', 'Gruntfile.js', 'configuration.json', 'package.json'],
+    tasks: [ 'full-build' ]
+  },
   bower: {
     files: [ 'bower_components/*' ],
     tasks: [ 'bower-dependencies' ]
